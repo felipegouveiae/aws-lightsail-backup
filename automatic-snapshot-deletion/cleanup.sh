b5ca089d754456af117af03a5e08df87f5a4ee7f
Felipe Esteves <felipegouveiae@gmail.com>
2021-10-23 15:44:18 -0300

    aws-sdk implemented to retrieve snapshots

--- automatic-snapshot-deletion/cleanup.sh
+++ automatic-snapshot-deletion/cleanup.sh
@@ -7,3 +7,4 @@ max_date_timestamp=$(date  -v-$DATE_RETAIN_EXPRESSION    +"%s")
  
  echo "deleting snapshots older than $max_date_string - max timestamp: $max_date_timestamp"
  
+ aws lightsail get-instance-snapshots --region us-east-1 --max-items 20
